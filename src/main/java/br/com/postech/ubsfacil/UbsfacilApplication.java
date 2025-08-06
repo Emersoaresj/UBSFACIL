9985bef26bffbf98e9473526c6f7666a81e701fe
Emersoaresj <emersonsoares269@icloud.com>
2025-08-06 20:45:08 -0300

    primeiro commit

---
+++ src/main/java/br/com/postech/ubsfacil/UbsfacilApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.ubsfacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsfacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbsfacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postech/ubsfacil/UbsfacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.ubsfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbsfacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
